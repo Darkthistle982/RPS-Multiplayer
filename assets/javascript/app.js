1c38eb155369b6407f432d69f97fa835de92e318
Brian Allen <brianallen2401@gmail.com>
2020-02-05 15:30:01 -0700

    added firebase config object to initialize database

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+//config variable and code to initialize the database. pulled from firebase docs
+var config = {
+    apiKey: "AIzaSyAAiTCPOS2fCQqyqcyVB4NU5q9SQdoIsC8",
+    authDomain: "rps-live-71692.firebaseapp.com",
+    databaseURL: "https://rps-live-71692.firebaseio.com",
+    projectId: "rps-live-71692",
+    storageBucket: "rps-live-71692.appspot.com",
+    messagingSenderId: "736046736181",
+};

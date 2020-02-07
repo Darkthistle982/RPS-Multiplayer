bd8dcdddf64b2d8b04da129daf0c4bb3102ce7c6
Brian Allen <brianallen2401@gmail.com>
2020-02-06 19:51:33 -0700

    redid the config info to connect to firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,14 @@
 //config variable and code to initialize the database. pulled from firebase docs
-var config = {
+const firebaseConfig = {
     apiKey: "AIzaSyAAiTCPOS2fCQqyqcyVB4NU5q9SQdoIsC8",
     authDomain: "rps-live-71692.firebaseapp.com",
     databaseURL: "https://rps-live-71692.firebaseio.com",
     projectId: "rps-live-71692",
     storageBucket: "rps-live-71692.appspot.com",
     messagingSenderId: "736046736181",
-};
-firebase.initializeApp(config);
+    appId: "1:736046736181:web:2f2dd2bb15f56b311de27c"
+  };
+  firebase.initializeApp(firebaseConfig);
 //variables needed for our game go here
 //-----------------------------------------------------------------------------------------
 var database = firebase.database();
@@ -30,11 +31,6 @@ var whoAmI = "none";
 //-----------------------------------------------------------------------------------------
 
 
-
-//Main Process goes here
-//-----------------------------------------------------------------------------------------
-
-
-
-
+    
+    
 

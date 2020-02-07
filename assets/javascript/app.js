6348f89e6ef0da3f8c2c5a6caf2520fd5462e2d6
Brian Allen <brianallen2401@gmail.com>
2020-02-07 13:24:01 -0700

    changed the add to db method to update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,14 @@
 //config variable and code to initialize the database. pulled from firebase docs
 const firebaseConfig = {
-    apiKey: "AIzaSyAAiTCPOS2fCQqyqcyVB4NU5q9SQdoIsC8",
-    authDomain: "rps-live-71692.firebaseapp.com",
-    databaseURL: "https://rps-live-71692.firebaseio.com",
-    projectId: "rps-live-71692",
-    storageBucket: "rps-live-71692.appspot.com",
-    messagingSenderId: "736046736181",
-    appId: "1:736046736181:web:2f2dd2bb15f56b311de27c"
-  };
-  firebase.initializeApp(firebaseConfig);
+  apiKey: "AIzaSyAAiTCPOS2fCQqyqcyVB4NU5q9SQdoIsC8",
+  authDomain: "rps-live-71692.firebaseapp.com",
+  databaseURL: "https://rps-live-71692.firebaseio.com",
+  projectId: "rps-live-71692",
+  storageBucket: "rps-live-71692.appspot.com",
+  messagingSenderId: "736046736181",
+  appId: "1:736046736181:web:2f2dd2bb15f56b311de27c"
+};
+firebase.initializeApp(firebaseConfig);
 //variables needed for our game go here
 //-----------------------------------------------------------------------------------------
 var database = firebase.database();
@@ -28,20 +28,25 @@ var p2Choice;
 var playerTurn;
 //functions & events go here
 //-----------------------------------------------------------------------------------------
-$(".p1-button").on("click", function() {
-$("#player1-choice").empty();
-p1Choice = $(this).val();
-console.log(p1Choice);
-var p1Display = $("<p>").text("Player 1 chose: " + p1Choice);
-$("#player1-choice").append(p1Display);
-})
+$(".p1-button").on("click", function () {
+  $("#player1-choice").empty();
+  p1Choice = $(this).val();
+  console.log(p1Choice);
+  // var p1Display = $("<p>").text("Player 1 chose: " + p1Choice);
+  // $("#player1-choice").append(p1Display);
+  database.ref().update({
+    p1Picks: p1Choice 
+  });
+});
 
-$(".p2-button").on("click", function() {
+$(".p2-button").on("click", function () {
   $("#player2-choice").empty();
   p2Choice = $(this).val();
   console.log(p2Choice);
-  var p2Display = $("<p>").text("Player 2 chose: " + p1Choice);
-  $("#player2-choice").append(p2Display);
-  })    
-    
+  // var p2Display = $("<p>").text("Player 2 chose: " + p1Choice);
+  // $("#player2-choice").append(p2Display);
+  database.ref().update({
+    p2Picks: p2Choice
+  });
+});
 

fb2dbf7885ef2bc234518b31a3d2f0f26cb54cd3
Brian Allen <brianallen2401@gmail.com>
2020-02-08 18:55:19 -0700

    added in logic to clear the results to reset function. added in display of winner or tie in results. set a timeout to run the reset funciton 5 seconds after the game logic is completed.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -31,6 +31,8 @@ var scoreLogged = false;
 function reset() {
   p1Choice = initialChoice;
   p2Choice = initialChoice;
+  scoreLogged = false;
+  $("#results").empty();
   database.ref().update({
     p1Choice: initialChoice,
     p2Choice: initialChoice,
@@ -38,23 +40,27 @@ function reset() {
   watchForSnapshot();
 }
 reset();
-
 function game() {
-
+  var player1Wins = $("<p>").text("Player 1 is the winner!");
+  var player2Wins = $("<p>").text("Player 2 is the winner!");
   if ((p1Choice === "Rock" && p2Choice === "Scissors") ||
     (p1Choice === "Scissors" && p2Choice === "Paper") ||
     (p1Choice === "Paper" && p2Choice === "Rock")) {
     p1Wins++;
     p2Losses++;
+    $("#results").append(player1Wins);
+
   }
   else if ((p2Choice === "Rock" && p1Choice === "Scissors") ||
     (p2Choice === "Scissors" && p1Choice === "Paper") ||
     (p2Choice === "Paper" && p1Choice === "Rock")) {
     p2Wins++;
     p1Losses++;
+    $("#results").append(player2Wins);
   }
   else {
     ties++;
+    $("#results").text("That's a tie!!")
   }
   database.ref().update({
     p1Wins: p1Wins,
@@ -63,8 +69,8 @@ function game() {
     p2Losses: p2Losses,
     ties: ties
   });
+  setTimeout(reset, 5 * 1000);
 }
-// game();
 //onclick function to log p1 choices to the db
 $(".p1-button").on("click", function () {
   p1Choice = $(this).val();
@@ -86,6 +92,7 @@ function watchForSnapshot() {
   database.ref().on("value", function (snapshot) {
     p1Choice = snapshot.val().p1Choice;
     p2Choice = snapshot.val().p2Choice;
+
     var p2Display = $("<p>").text("Player 2 chose: " + p2Choice);
     $("#player2-choice").html(p2Display);
     var p1Display = $("<p>").text("Player 1 chose: " + p1Choice);
